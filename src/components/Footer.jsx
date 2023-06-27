f5cf425f8072ed60063e238fc3135eb72fe285c8
Misbah Uddin Tareq <lumisbah92@gmail.com>
2023-06-27 19:24:36 +0600

    updated readme

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a href="https://www.youtube.com/@lumisbah92" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.facebook.com/lumisbah92" target={"blank"}>
+            Facebook
+          </a>
+          <a href="https://github.com/lumisbah92" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+
+export default Footer;
